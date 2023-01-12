00b68e50c525db0a88c0a2a928f7c7274bed4704
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-01-12 14:33:01 -0700

    Add styles

--- src/components/AllStationsContainer/AllStationsContainer.tsx
+++ src/components/AllStationsContainer/AllStationsContainer.tsx
@@ -1,9 +1,10 @@
+import "./AllStationsContainer.scss";
 import { GasStation } from "../../utilities/types";
 import { StationCard } from "../StationCard/StationCard";
 
 export const AllStationsContainer = (props: any) => {
   return (
-    <div>
+    <div className="all-station-container">
       {props.allStations.map((station: GasStation) => {
         return <StationCard key={station.cid} {...station} />;
       })}
--- src/components/StationCard/StationCard.tsx
+++ src/components/StationCard/StationCard.tsx
@@ -1,11 +1,16 @@
+import logo from "../../images/logo-single.png";
 import "./StationCard.scss";
 import { GasStation, GasPrice } from "../../utilities/types";
+import { Link } from "react-router-dom";
 
 export const StationCard = (props: GasStation) => {
   return (
     <div className="single-station-card">
       <div>
-        <h1>{props.title}</h1>
+        <Link id="link-name" to={`/details/${props.cid}`}>
+          <img src={logo} alt="logo" />
+          <h2>{props.title}</h2>
+        </Link>
         <p>{props.address}</p>
       </div>
       <div>
