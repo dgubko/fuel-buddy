57bd3677556cf31be40239d20113fe107d96a648
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-01-12 17:05:00 -0700

    Add Form

--- src/components/AllStationsContainer/AllStationsContainer.tsx
+++ src/components/AllStationsContainer/AllStationsContainer.tsx
@@ -1,10 +1,21 @@
 import "./AllStationsContainer.scss";
 import { GasStation } from "../../utilities/types";
 import { StationCard } from "../StationCard/StationCard";
+import { Form } from "../Form/Form";
+import { useState } from "react";
+import { GasTypes } from "../../utilities/types";
 
 export const AllStationsContainer = (props: any) => {
+  const [location, setLocation] = useState<string>("");
+  const [fuelType, setFuelType] = useState<GasTypes>("Regular");
   return (
     <div className="all-station-container">
+      <Form
+        location={location}
+        fuelType={fuelType}
+        setFuelType={setFuelType}
+        setLocation={setLocation}
+      />
       {props.allStations.map((station: GasStation) => {
         return <StationCard key={station.cid} {...station} />;
       })}
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,38 @@
+import { GasTypes } from "../../utilities/types";
+
+type Props = {
+  location: string;
+  fuelType: GasTypes;
+  setFuelType: (value: GasTypes) => void;
+  setLocation: (value: string) => void;
+};
+
+export const Form = (props: Props) => {
+  const handleFuelTypeChange = (event: any) => {
+    props.setFuelType(event.target.value);
+  };
+
+  const handleLocationChange = (event: any) => {
+    props.setLocation(event.target.value);
+  };
+  return (
+    <form className="inputs">
+      <input
+        placeholder="Enter your city"
+        value={props.location}
+        onChange={handleLocationChange}
+      />
+      <select
+        id="fuel-type"
+        name="fuel-type"
+        value={props.fuelType}
+        onChange={handleFuelTypeChange}
+      >
+        <option value="Regular">Regular fuel prices</option>
+        <option value="Midgrade">Mid-grade fuel prices</option>
+        <option value="Premium">Premium fuel prices</option>
+        <option value="Diesel">Diesel fuel prices</option>
+      </select>
+    </form>
+  );
+};
