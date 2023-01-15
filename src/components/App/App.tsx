9cf84b3ca99168ce38c70b56c0da74fec84a1818
Dmitrii Gubko <dalnoboy29region@gmail.com>
2023-01-15 11:31:43 -0700

    Add favorite array and favorite functionality

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,30 @@
 import "./App.scss";
 import logo from "../../images/logo-new.svg";
-import { useEffect, useState } from "react";
+import { Factory, useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { getGasStations } from "../../apiCalls/gasStations";
 import { AllStationsContainer } from "../AllStationsContainer/AllStationsContainer";
-import { GasStation } from "../../utilities/types";
+import { Favorite, GasStation } from "../../utilities/types";
 import { DetailsPage } from "../DetailsPage/DetailsPage";
 import { Page404 } from "../Page404/Page404";
+import { Favorites } from "../Favorites/Favorites";
+import { Link } from "react-router-dom";
 
 function App() {
   const [allStations, setAllStations] = useState<GasStation[]>([]);
+  const [favorites, setFavorites] = useState<Favorite[]>([]);
+
+  const addToFav = (newItem: Favorite) => {
+    setFavorites([...favorites, newItem]);
+  };
+
+  const removeFromFav = (id: string) => {
+    setFavorites(
+      favorites.filter((item) => {
+        return item.cid !== id;
+      })
+    );
+  };
 
   useEffect(() => {
     getGasStations().then((data) => {
@@ -25,15 +40,37 @@ function App() {
     <div className="App">
       <header>
         <img src={logo} alt="Fuel Buddy Logo" />
+        <Link to="/favorites">Favorites</Link>
+        <Link to="/">All Stations</Link>
       </header>
       <Routes>
         <Route
           path="/"
-          element={<AllStationsContainer allStations={allStations} />}
+          element={
+            <AllStationsContainer
+              allStations={allStations}
+              removeFromFav={removeFromFav}
+              favorites={favorites}
+              addToFav={addToFav}
+            />
+          }
         />
         <Route
           path="/details/:id"
-          element={<DetailsPage allStations={allStations} />}
+          element={
+            <DetailsPage
+              allStations={allStations}
+              favorites={favorites}
+              addToFav={addToFav}
+              removeFromFav={removeFromFav}
+            />
+          }
+        />
+        <Route
+          path="/favorites"
+          element={
+            <Favorites favorites={favorites} removeFromFav={removeFromFav} />
+          }
         />
         <Route path="*" element={<Page404 />} />
       </Routes>
